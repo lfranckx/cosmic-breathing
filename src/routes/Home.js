74575ad4707934b2a3603860d7da1685e5f077bd
Bowie <lfranckx@thinkful.com>
2023-01-06 11:26:33 -0600

    add styling and vars scss file

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -3,14 +3,14 @@ import logo from '../images/logo-main.png';
 
 export default function Home() {
     return (
-        <>
+        <div class='page_wrap home'>
             <header id='header'>
                 <div className='flex container'>
                     <div class='logo_wrap'>
-                        <img src={logo} alt='Main logo for Cosmic Breathing' />
+                        <img src={logo} alt='Main logo for Cosmic Breathing' className='logo' />
                     </div>
                 </div>
             </header>
-        </>
+        </div>
     )
 }
