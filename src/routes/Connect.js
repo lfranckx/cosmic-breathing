e600e2482886f549525675d33d00c0c6dd1d22d2
Bowie <lfranckx@thinkful.com>
2023-01-19 15:37:16 -0600

    start connect page

--- src/routes/Connect.js
+++ src/routes/Connect.js
@@ -1,5 +1,15 @@
 import React from 'react';
 
 export default function Connect() {
-    return (<></>);
+    return (
+    <>
+        <div className='page_width'>
+            <main id='connect'>
+                <section className='content'>
+                    <h2 className='h1'>Connect</h2>
+                </section>
+            </main>
+        </div>
+    </>
+    );
 }
