42a0c9ab52819130648f189393b6aa1be79da72a
Bowie <lfranckx@thinkful.com>
2023-01-23 13:51:26 -0600

    add hexagon animations

---
+++ src/components/Animation.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import '../styles/Animation.scss';
+
+export default function Animation(props) {
+    return (
+        <div className='page-wrap'>
+            <div className='content-wrap'>
+                <div className='hex-wrap left'>
+                    <svg className='hex-peach' width="260" height="298" viewBox="0 0 260 298" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M125 4.19616C128.094 2.40983 131.906 2.40982 135 4.19615L252.904 72.2679C255.998 74.0543 257.904 77.3555 257.904 80.9282V217.072C257.904 220.644 255.998 223.946 252.904 225.732L135 293.804C131.906 295.59 128.094 295.59 125 293.804L7.0962 225.732C4.00218 223.946 2.09619 220.644 2.09619 217.072V80.9282C2.09619 77.3556 4.00218 74.0543 7.09619 72.2679L125 4.19616Z" stroke="#E7B9B9" stroke-opacity="0.4" stroke-width="4"/>
+                    </svg>
+                </div>
+                <div className='hex-wrap top'>
+                    <svg className='hex-blue' width="260" height="298" viewBox="0 0 260 298" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M135 4.19615L252.904 72.2679C255.998 74.0543 257.904 77.3555 257.904 80.9282V217.072C257.904 220.644 255.998 223.946 252.904 225.732L135 293.804C131.906 295.59 128.094 295.59 125 293.804L7.0962 225.732C4.00218 223.946 2.09619 220.644 2.09619 217.072V80.9282C2.09619 77.3556 4.00218 74.0543 7.09619 72.268L125 4.19616C128.094 2.40983 131.906 2.40982 135 4.19615Z" stroke="#000080" stroke-opacity="0.12" stroke-width="4"/>
+                    </svg>
+                </div>
+                <div className='hex-wrap right'>
+                    <svg className='hex-babyblue' width="260" height="298" viewBox="0 0 260 298" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M125 4.19616C128.094 2.40983 131.906 2.40982 135 4.19615L252.904 72.2679C255.998 74.0543 257.904 77.3555 257.904 80.9282V217.072C257.904 220.644 255.998 223.946 252.904 225.732L135 293.804C131.906 295.59 128.094 295.59 125 293.804L7.0962 225.732C4.00218 223.946 2.09619 220.644 2.09619 217.072V80.9282C2.09619 77.3556 4.00218 74.0543 7.09619 72.2679L125 4.19616Z" stroke="#B8D1CD" stroke-opacity="0.4" stroke-width="4"/>
+                    </svg>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../styles/Home.scss';
 import { motion } from "framer-motion";
+import Animation from '../components/Animation';
 
 export default function Home(props) {
 
@@ -10,26 +11,31 @@ export default function Home(props) {
     }
 
     return (
-        <motion.div 
-            initial="hidden"
-            animate="visible"
-            variants={variants}
-        >
-            <main id='home'>
-                <div className='small_width'>
-                    <div className='flex column-reverse'>
-                        <h1>Cosmic Breathing</h1>
-                        <h2>
-                            <span>Vedantic Breathwork</span>
-                            <span className='space'> • </span>
-                            <span>Meditation</span>
-                            <span className='space'> • </span>
-                            <span>Yoga in the Himalayan Tradition</span>
-                        </h2>
+        <>
+            <motion.div 
+                initial="hidden"
+                animate="visible"
+                variants={variants}
+                className='home_wrap'
+            >
+                <main id='home'>
+                    <div className='small_width'>
+                        <div className='flex column-reverse'>
+                            <h1>Cosmic Breathing</h1>
+                            <h2>
+                                <span>Vedantic Breathwork</span>
+                                <span className='space'> • </span>
+                                <span>Meditation</span>
+                                <span className='space'> • </span>
+                                <span>Yoga in the Himalayan Tradition</span>
+                            </h2>
+                        </div>
+                        <h2 className='text-center'>Integrated Approach to Personal Expansion</h2>
                     </div>
-                    <h2 className='text-center'>Integrated Approach to Personal Expansion</h2>
-                </div>
-            </main>
-        </motion.div>
+                </main>
+            </motion.div>
+            <Animation />
+        </>
+        
     );
 }
