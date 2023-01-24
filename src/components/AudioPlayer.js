677853a95ce1ee18004d9fcaf62ab8895ece37a5
Bowie <lfranckx@thinkful.com>
2023-01-23 20:49:31 -0600

    pause button styling udpate

--- src/components/AudioPlayer.js
+++ src/components/AudioPlayer.js
@@ -1,20 +1,23 @@
 import React, { useState, useRef } from 'react';
 import music from '../misc/cosmic-breathing.mp4';
+import { useLocation } from 'react-router';
 
 export default function AudioPlayer(props) {
+
+    const location = useLocation();
+    const homeTextColor = location.pathname === '/' ? 'p1' : 'p1';
+    const aboutTextColor = location.pathname === '/about' ? 'p1 bronze' : 'p1';
+    const servicesTextColor = location.pathname === '/services' ? 'p1' : 'p1';
+    const connectTextColor = location.pathname === '/connect' ? 'p1' : 'p1';
+
 
     const audioRef = useRef(null);
     const [isMuted, setIsMuted] = useState(false);
     const [isPlaying, setIsPlaying] = useState(false);
-    const [buttonText, setButtonText] = useState("Pause")
 
     const toggleMute = () => {
         setIsMuted(!isMuted);
         audioRef.current.muted = !isMuted;
-        if (isMuted) {
-            setButtonText("Play");
-        } 
-        if (!isMuted) { setButtonText("Pause"); }
     }
 
     const togglePlaying = () => {
@@ -25,7 +28,12 @@ export default function AudioPlayer(props) {
     return (
         <div className='page-width audio-wrap'>
             <audio ref={audioRef} src={music} controls autoPlay loop/>
-            <button onClick={togglePlaying} className='btn-mute'>{isPlaying ? "Pause" : "Play"}</button>
+            <button onClick={togglePlaying} 
+                className={`btn-mute ${homeTextColor} ${aboutTextColor} ${servicesTextColor} ${connectTextColor}`}
+            >{isPlaying ? "Pause" : "Play"}
+            <div className={location.pathname === '/about' ? 'border-bottom bronze' : 'border-bottom'}></div>
+            </button>
+            
         </div>
     );
 }
